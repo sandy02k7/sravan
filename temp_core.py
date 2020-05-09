8f5fed52c4a9e088e44e66be222e2e1463ce6592
sravan_ubuntu <mm18m011@smail.iitm.ac.in>
2020-05-09 19:19:07 +0530

    k value has been kept constant and data file has been modified to csv

--- temp_core.py
+++ temp_core.py
@@ -7,10 +7,12 @@ def Cp(T):
 #        return 470 + (0.02*T) + (0.00038*T*T)
     
 def K(T):
-    if(T <= 800):
-            return 54 - 0.0333*T
-    else:
-            return 27.3 
+		return 45
+
+#    if(T <= 800):
+#           return 54 - 0.0333*T
+#  else:
+#            return 27.3 
 #        return 52.27 - (0.01541*T) - (0.00002155*T*T)
 
 def transpose(l1, l2): 
@@ -23,7 +25,6 @@ def transpose(l1, l2):
     return l2 
 
 arr = []
-arr_t = []
 dr = 0.0004
 R = 0.0080
 Rc = 0.014 
@@ -34,7 +35,8 @@ Qno = Qtot/no
 p = 2*3.14*((Rc + R)/2)
 A = 3.14*(Rc*Rc - R*R)
 n = int(R/dr)
-time_taken = np.ones(n)*0;
+time_taken = 0
+time_taken_arr = []
 T = np.ones(n)*1000
 Tf = np.ones(n)
 Dh = 4*A/p
@@ -69,60 +71,60 @@ for t in range(225, 1000):
 	dtr = 2*x*Cp(t)/((2*K(t)/y) - (K(t)/z) + (wbR))
 	dt = min(dt, min(dt0, min(dtr, dtR)))
 
-itr = int(1e8)
+itr = int(1e3)
 for i in range(itr):
 
 	vec = []
-	
+
 	cp0 = Cp(T[0])
 	k0 = K(T[0])
-	time_taken[0] = time_taken[0] + dt
 	a = k0/y
 	c0 = (ro*cp0/(dt)) - (4*k0/y)
 	d = dt/(ro*cp0)
 	Tf[0] = (4*a*T[1] + c0*T[0])*d
 	if(i%(itr/100) == 0):
-		vec.append(tuple((Tf[0], time_taken[0])))
+		vec.append(Tf[0])
 
 	for j in range(1, n-1):
 		cpr = Cp(T[j])
 		kr = K(T[j])
-		time_taken[j] = time_taken[j] + dt
 		a = kr/y
 		b = kr/(2*z)
 		cr = ro*cpr/(dt) - 2*kr/(y)
 		d = (dt/(ro*cpr))
 		Tf[j] = (a*(T[j-1] + T[j+1]) + b*(T[j-1] - T[j+1]) + cr*(T[j]))*d
 		if(i%(itr/100) == 0):
-			vec.append(tuple((Tf[j], time_taken[j])))
+			vec.append(Tf[j])
 
-	if(time_taken[n-1] < time_thr):
+	if(time_taken < time_thr):
 		bR = wbR
 	else:
 		bR = abR
 	cpR = Cp(T[n-1])
 	kR = K(T[n-1])
-	time_taken[n-1] = time_taken[n-1] + dt
 	aR = 2*kR/(y) - kR/(z)
 	cR = ro*cpR/(dt) - (aR) - (bR)
 	d = dt/(ro*cpR)
 	Tf[n-1] = (aR*T[n-2] + bR*T_water_air + cR*T[n-1])*d
 	if(i%(itr/100) == 0):
-		vec.append(tuple((Tf[n-1], time_taken[n-1])))
+		vec.append(Tf[n-1])
 
 	T = Tf
+	time_taken  = time_taken + dt
 
 	if(i%(itr/100) == 0):
 		arr.append(vec);
+		time_taken_arr.append(time_taken)
 		now = datetime.datetime.now()
-		print(Tf[n-1], i/(itr/100), time_taken[n-1], now.strftime("%Y-%m-%d %H:%M:%S"))
+		print(Tf[n-1], i/(itr/100), time_taken, now.strftime("%Y-%m-%d %H:%M:%S"))
 
-filename = 'data.txt'
+filename = 'data.csv'
 with open(filename, 'w') as f:
-	arr_t = transpose(arr, arr_t)
-	for ind in range(len(arr_t)):
-		print(arr_t[ind], file=f)  # Python 3.x
+	for ind in range(len(arr)):
+		print(arr[ind], file=f)  # Python 3.x
+	print(time_taken_arr, file=f)  # Python 3.x
 
-#plt.plot(range(len(arr)), arr)
-#plt.ylabel('Temperature')
-#plt.show(block=True)
+plt.plot(range(len(arr[n-1])), arr[n-1])
+plt.xlabel('time')
+plt.ylabel('Temperature')
+plt.show(block=True)
